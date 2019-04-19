225120e64b70302e17fc1a06853edd4c7cc9610c
Alexander <easuazab@co.ibm.com>
2019-04-19 08:35:03 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        let salida = {
+            nombre: 'Alexander',
+            edad: '32',
+            url: req.url
+        }
+
+        res.write(JSON.stringify(salida));
+        //res.write('Hola Mundo');
+        res.end();
+
+    })
+    .listen(8080);
+
+console.log('Runing on port 8080');
